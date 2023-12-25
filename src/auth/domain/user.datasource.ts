193a8aca7537afbedffbd24ada211537df086a44
gustavocadev <gustavocadev@gmail.com>
2023-12-25 16:09:18 -0500

    Add login impl

--- src/auth/domain/user.datasource.ts
+++ src/auth/domain/user.datasource.ts
@@ -5,6 +5,11 @@ export abstract class UserDatasource {
   // all these names are on own, here I need to think all the methods my business needs
   abstract findUserByIdImpl(uuid: string): Promise<UserEntity | null>;
 
+  abstract loginImpl(
+    email: string,
+    password: string
+  ): Promise<UserEntity | null>;
+
   abstract registerUserImpl(user: UserEntity): Promise<UserEntity | null>;
 
   abstract listUserImpl(): Promise<UserEntity[] | null>;
