13ec01ed5394236c393c8a35fd015c308a0d38f0
gustavocadev <gustavocadev@gmail.com>
2023-12-25 16:36:52 -0500

    Remove unused methods

--- src/auth/domain/user.repository.ts
+++ src/auth/domain/user.repository.ts
@@ -1,15 +1,12 @@
 import { UserEntity } from './user.entity.ts';
 
+// to use in controller.use-cases.ts and use user.repository.impl.ts in infrastructure
 export abstract class UserRepository {
   // all these names are on own, here I need to think all the methods my business needs
-  abstract findUserById(uuid: string): Promise<UserEntity | null>;
-
   abstract loginUser(
     email: string,
     password: string
   ): Promise<UserEntity | null>;
 
   abstract registerUser(user: UserEntity): Promise<UserEntity | null>;
-
-  abstract listUser(): Promise<UserEntity[] | null>;
 }
--- src/auth/infrastructure/user.repository.impl.ts
+++ src/auth/infrastructure/user.repository.impl.ts
@@ -5,19 +5,11 @@ import { UserDatasource } from '../domain/user.datasource.ts';
 export class UserRepositoryImpl implements UserRepository {
   constructor(private readonly userDatasource: UserDatasource) {}
 
-  findUserById(uuid: string): Promise<UserEntity | null> {
-    return this.userDatasource.findUserByIdImpl(uuid);
-  }
-
   loginUser(email: string, password: string): Promise<UserEntity | null> {
     return this.userDatasource.loginImpl(email, password);
   }
 
   registerUser(user: UserEntity): Promise<UserEntity | null> {
     return this.userDatasource.registerUserImpl(user);
-  }
-
-  listUser(): Promise<UserEntity[] | null> {
-    return this.userDatasource.listUserImpl();
   }
 }
