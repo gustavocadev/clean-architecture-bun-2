bc48ccbafda646a3f72064bc2c813cea03db0657
gustavocadev <gustavocadev@gmail.com>
2023-12-25 16:09:40 -0500

    Add login method

--- src/auth/domain/user.repository.ts
+++ src/auth/domain/user.repository.ts
@@ -4,6 +4,11 @@ export abstract class UserRepository {
   // all these names are on own, here I need to think all the methods my business needs
   abstract findUserById(uuid: string): Promise<UserEntity | null>;
 
+  abstract loginUser(
+    email: string,
+    password: string
+  ): Promise<UserEntity | null>;
+
   abstract registerUser(user: UserEntity): Promise<UserEntity | null>;
 
   abstract listUser(): Promise<UserEntity[] | null>;
